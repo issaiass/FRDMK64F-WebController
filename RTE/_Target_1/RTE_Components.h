59ad13e1eb040c98f5ce7fa6973f642e8cc53ce4
Rangel Alvarado <issaiass@hotmail.com>
2018-09-08 16:29:04 -0500

    RTOS with LED Only Working

---
+++ RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,38 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'WebController' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "fsl_device_registers.h"
+
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
+#define RTE_DEVICE_CONFIGURATION_FRDM_K64F
+#define RTE_DEVICE_DRIVER_GPIO
+#define RTE_DEVICE_DRIVER_RTC
+#define RTE_DEVICE_HAL_GPIO
+#define RTE_DEVICE_HAL_LPTMR
+#define RTE_DEVICE_HAL_MCG
+#define RTE_DEVICE_HAL_OSC
+#define RTE_DEVICE_HAL_PORT
+#define RTE_DEVICE_HAL_RTC
+#define RTE_DEVICE_HAL_SIM
+#define RTE_DEVICE_HAL_UART
+#define RTE_DEVICE_OSA_BM
+#define RTE_DEVICE_STARTUP_KINETIS      /* Device Startup for Kinetis Series */
+#define RTE_DEVICE_SYSTEM_CLOCK
+#define RTE_DEVICE_SYSTEM_INTERRUPT
+#define RTE_DEVICE_UTILITY_DEBUG_CONSOLE
+
+#endif /* RTE_COMPONENTS_H */
---
+++ main.c
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------------
+ * CMSIS-RTOS 'main' function template
+ *---------------------------------------------------------------------------*/
+
+#define osObjectsPublic                     // define objects in main module
+#include "osObjects.h"                      // RTOS object definitions
+#include "Board_LED.h"                  // ::Board Support:LED
+
+
+extern void hardware_init (void);
+//extern void Init_Thread(void);
+
+/*
+ * main: initialize and start the system
+ */
+int main (void) {
+ osKernelInitialize(); // initialize CMSIS-RTOS
+ hardware_init();
+ LED_Initialize();
+ osKernelStart(); // start thread execution
+
+while(1) {
+ osDelay(500);
+ LED_On(1); //red LED
+ osDelay(500);
+ LED_Off(1);
+ }
+}
